6d56edfa22cbf7d2b8e4c0c6f3767e0685def2d9
Derek Kastner <dkastner@gmail.com>
2017-04-13 19:45:05 -0500

    WIP

--- lib/rails-rack-session-cookie/rails.rb
+++ lib/rails-rack-session-cookie/rails.rb
@@ -5,10 +5,6 @@ module RailsRackSessionCookie
     initializer 'rails-rack-session-cookie.configure_rails_initialization' do
       # Remove the egregious rails cookie store
       app.middleware.insert_before ActionDispatch::Cookies,
-        Rack::Session::Cookie,
-        secret: SecretGarden.fetch!('SECRET_KEY_BASE'),
-        key: '_faraday_session',
-        domain: ENV.fetch('COOKIE_DOMAIN')
 
       app.middleware.delete ActionDispatch::Cookies
       app.middleware.delete ActionDispatch::Session::CookieStore
